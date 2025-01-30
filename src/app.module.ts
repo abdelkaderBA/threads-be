d28da03de08156b4b28a2a5990f96b525adb6bfa
abdelkaderBA <abdelkader.benaicha7@gmail.com>
2025-01-30 17:04:42 +0100

    cache mongoDB url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { CommentModule } from './comment/comment.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     UsersModule,
     CommentModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://threads-user:tWvmj1LAtLjjg82V@cluster0.trsrp.mongodb.net/threads?retryWrites=true&w=majority&appName=Cluster0',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
